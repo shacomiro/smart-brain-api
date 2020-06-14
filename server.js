fbbac71e3b95821d462cc4b34b504cc3f4673149
Sunghyun Lee <98dltjdgus@naver.com>
2020-06-14 21:51:26 +0900

    Add smart-brain back-end server files

---
+++ server.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+// 데이터베이스와 서버를 연결하기 위해 knex.js를 사용
+// https://knexjs.org/ 참조
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: '!2TiZhAlFh',
+    database: 'smart-brain',
+  },
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {});
+
+app.post('/signin', (req, res) => {
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+
+      if (isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', req.body.email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => {
+            res.status(400).json('unable to get user');
+          });
+      } else {
+        res.status(400).json('worng credentials');
+      }
+    })
+    .catch((err) => {
+      res.status(400).json('wrong credentials');
+    });
+});
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  var hash = bcrypt.hashSync(password);
+
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into('login')
+      .returning('email')
+      .then((loginEmail) => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json('unable to register'));
+});
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.select('*')
+    .from('users')
+    .where({
+      id: id,
+    })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json('Not found');
+      }
+    })
+    .catch((err) => {
+      res.status(400).json('error getting user');
+    });
+});
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entreis) => {
+      res.json(entreis[0]);
+    })
+    .catch((err) => {
+      res.status(400).json('unable to get entries');
+    });
+});
+
+app.listen(3001, () => {
+  console.log('app is running on port 3001');
+});
