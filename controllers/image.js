8e6c3294cea8d9d220a7ef2677558991d9481d93
Sunghyun Lee <98dltjdgus@naver.com>
2020-06-17 21:31:50 +0900

    Update for back-end security

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'aa4e641e96464328af75e13196a5c6bf',
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json('unable to work with API'));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
 
@@ -15,4 +30,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage: handleImage,
+  handleApiCall: handleApiCall,
 };
--- server.js
+++ server.js
@@ -38,6 +38,9 @@ app.get('/profile/:id', (req, res) => {
 app.put('/image', (req, res) => {
   image.handleImage(req, res, db);
 });
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 app.listen(3001, () => {
   console.log('app is running on port 3001');
