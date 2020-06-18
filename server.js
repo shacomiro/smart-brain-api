34d9079bd6498a1fd86bd8f9f35bac535ba2388d
Sunghyun Lee <98dltjdgus@naver.com>
2020-06-18 22:29:44 +0900

    Update server.js for adding port

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ app.post('/imageurl', (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(3001, () => {
-  console.log('app is running on port 3001');
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 });
