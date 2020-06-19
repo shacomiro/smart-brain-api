1c8ea1b1d6514f6611bc3ae7828c4a083919a2c4
Sunghyun Lee <98dltjdgus@naver.com>
2020-06-19 23:19:07 +0900

    Update database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-reticulated-52933',
     user: 'postgres',
     password: '!2TiZhAlFh',
     database: 'smart-brain',
